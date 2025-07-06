ada1a9155554acc5e72c0cb3cab444416192c535
R1FA7 <minhazulkabir510@gmail.com>
2025-07-06 19:34:25 +0600

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelectorAll(".nav-link");
   const sections = document.querySelectorAll("section");
 
-  // Function to set the active link based on scroll position
+  // set the active link based on scroll position
   function setActiveLinkOnScroll() {
-    let scrollPosition = window.scrollY + 200; // Adjusted offset for better detection
-
+    let scrollPosition = window.scrollY + 200; 
     sections.forEach((section) => {
       const sectionTop = section.offsetTop;
       const sectionBottom = sectionTop + section.offsetHeight;
 
-      // Check if the current scroll position is within this section's range
+      // current scroll position is within this section's range?
       if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
         // Remove the active class from all links
         navLinks.forEach(nav => nav.classList.remove("active-link"));
@@ -24,26 +23,26 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Call the function on scroll to update active link based on scroll position
+  // Call scroll to update active link based on scroll position
   window.addEventListener("scroll", setActiveLinkOnScroll);
 
-  // Initialize the first active link (About section) when the page loads
+  // Initialize
   document.querySelector('.nav-link[href="#about"]').classList.add('active-link');
 
-  // Function to handle the click on navigation links
+  //handle the click on navigation links
   navLinks.forEach(link => {
     link.addEventListener("click", (event) => {
       // Prevent default behavior of the anchor tag
       event.preventDefault();
 
       // Get the section to scroll to
-      const sectionId = event.target.getAttribute("href").substring(1); // Get the section ID (e.g., "#about" -> "about")
+      const sectionId = event.target.getAttribute("href").substring(1);
       const section = document.getElementById(sectionId);
 
       // Smooth scroll to the section
       section.scrollIntoView({
         behavior: "smooth",
-        block: "start", // Scroll to the top of the section
+        block: "start", 
       });
 
       // Delay the addition of the active class to allow for smooth scrolling to complete
@@ -53,10 +52,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Add the active class to the clicked link
         event.target.classList.add("active-link");
-      }, 300); // Delay in milliseconds (should match the scroll duration)
+      }, 300);
     });
   });
 
-  // Ensure active link is set based on scroll position when the page loads
+  // page loads Now!!!
   setActiveLinkOnScroll();
 });
